354387674ec982466294665b6b3738dc0d14f3e8
john ontita <johnontita2000@gmail.com>
2023-03-31 18:14:20 +0300

    using lenth in arrays

---
+++ app.js
@@ -0,0 +1,12 @@
+// const fruits =[];
+// //.push is used to add
+// fruits.push("banana","mango","orange","pineapple");
+// // .pop is used to remove the last element in array
+
+// fruits.pop("pineapple");
+// console.log(fruits);
+
+const fruits=["oranges","watermelon","wauvas","pumpkin","coconut","cashewnuts"];
+fruits.push("plums");
+fruits.push("avocado");
+console.log(fruits.length);
